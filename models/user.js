e7ea8db5c046999bfc45fada04d68cdf77f2dd99
Karthick <inter_karthick@vertace.com>
2023-06-26 15:03:29 +0530

    Initial Push

---
+++ models/user.js
@@ -0,0 +1,27 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, "Email already exists!"],
+    required: [true, "Email is required!"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+    ],
+  },
+  image: {
+    type: String,
+  },
+});
+
+// In normal way, the connection will be always running and hits db each time it wants
+// But in nextJS it done only when it is needed to check is done before for the 
+//presence already more over like cache.
+const User = models.User || model("User", UserSchema);
+
+export default User;
